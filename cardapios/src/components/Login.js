4a5c53234a0fd82ef74e219b09850ae47a56bb6f
Beatriz Valio Weiss <beatriz.valio@gmail.com>
2022-02-13 20:40:09 -0300

    Login: design final + responsividade

--- cardapios/src/components/Login.js
+++ cardapios/src/components/Login.js
@@ -1,26 +1,26 @@
 import './Login.css';
 import ItemLogin from './ItemLogin';
-import React,{useState} from 'react';
+import React from 'react';
 
 function Login() {
   return (
     <div className='Login'>
 
       <div className="MenuLogin">
-        <label className="TituloLogin" >CARDÁPI.OS </label>
+        <a className="TituloLogin">CARDÁPI.OS </a>
         <ItemLogin name="Quem somos" />
         <ItemLogin name="Seja um parceiro" />
       </div>
     
       <div className="FormularioLogin">
 
-        <p>Faça seu login</p>
+        <h3>Faça seu login</h3>
         
         <form className='Formulario'>
 
           <div className='email'>
             <label for="email"> E-MAIL </label>
-            <input type="email" name="email" placeholder="Insira o email cadastrado" required />
+            <input type="email" name="email" id="email" placeholder="Insira o email cadastrado" required />
           </div>
 
           <div className='senha'>
@@ -28,7 +28,19 @@ function Login() {
             <input type="password" name="password" id="password" placeholder="Digite sua senha" required/>
           </div>
 
-          <button className='entrar'>ENTRAR</button>
+          <p id='alternativa'>OU</p>
+          <div className='loginAlternativo'>
+            <button className='loginRedeSocial'><img alt="" src="https://cdn.icon-icons.com/icons2/2631/PNG/512/google_search_new_logo_icon_159150.png" width="50" height="50"></img></button>
+            <button className='loginRedeSocial'><img alt="" src="https://i.pinimg.com/originals/2b/79/51/2b7951ca3923b28a9e77518b19d1ea46.png" width="50" height="50"></img></button>
+          </div>
+
+          <div className='iniciarCadastro'>
+            <p>Ainda não possui cadastro?</p>
+            <p className='linkCadastro'>Cadastre-se aqui!</p>
+            <button className='entrar'>ENTRAR</button>
+          </div>
+
+        
 
         </form>
       </div>
