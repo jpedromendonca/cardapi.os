86aa0bab3b38f39aa76af3b3316977db1dd0642e
jotapemd <joaopmdearaujo@gmail.com>
2022-02-13 20:38:04 -0300

    alteração de arquitetura

--- cardapios/src/components/Cadastro.js
+++ cardapios/src/components/Cadastro.js
@@ -1,34 +1,74 @@
-function Menu() {
+function Cadastro() {
   return (
     <div id="container">
-        <div class="item">
-            <div class="logo-nome">
-                <h1>Cardap.io</h1>
-                <img src="imagens/logo.jpeg"/>
-            </div>
-            <div class="menu">
-                <ul class="opcoes">
-                    <li><a href="#">Hambúrguers</a></li>
-                    <li><a href="#">Bebidas não alcoólicas</a></li>
-                    <li><a href="#">Bebidas alcoólicas</a></li>
-                    <li><a href="#">Porções e molhos</a></li>            
-                </ul>
-            </div>
-        </div> 
-        <div class="item a1">
-            <h1>Cadastre-se</h1>
-            <form class="formulario" action="">
-                <input type="text" name="nome-completo" class="inputs" id="nome-completo" placeholder="Nome completo">
-                <input type="text" name="cpf" class="inputs" id="cpf" placeholder="CPF">
-                <input type="text" name="telefone" class="inputs" id="telefone" placeholder="Telefone celular">
-                <input type="password" name="senha" class="inputs" id="senha" placeholder="Senha">
-                <input type="password" name="conf-senha" class="inputs" id="conf-senha" placeholder="Confirmacao senha">
-                <button class="botao-continuar">Continuar</button>                
-                <p class="descricao">Já possui conta?<a href="#"> Faça login</a></p>
-            </form>
+      <div className="item">
+        <div className="logo-nome">
+          <h1>Cardap.io</h1>
+          <img src="imagens/logo.jpeg" />
         </div>
+        <div className="menu">
+          <ul className="opcoes">
+            <li>
+              <a href="#">Hambúrguers</a>
+            </li>
+            <li>
+              <a href="#">Bebidas não alcoólicas</a>
+            </li>
+            <li>
+              <a href="#">Bebidas alcoólicas</a>
+            </li>
+            <li>
+              <a href="#">Porções e molhos</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="item a1">
+        <h1>Cadastre-se</h1>
+        <form className="formulario" action="">
+          <input
+            type="text"
+            name="nome-completo"
+            className="inputs"
+            id="nome-completo"
+            placeholder="Nome completo"
+          />
+          <input
+            type="text"
+            name="cpf"
+            className="inputs"
+            id="cpf"
+            placeholder="CPF"
+          />
+          <input
+            type="text"
+            name="telefone"
+            className="inputs"
+            id="telefone"
+            placeholder="Telefone celular"
+          />
+          <input
+            type="password"
+            name="senha"
+            className="inputs"
+            id="senha"
+            placeholder="Senha"
+          />
+          <input
+            type="password"
+            name="conf-senha"
+            className="inputs"
+            id="conf-senha"
+            placeholder="Confirmacao senha"
+          />
+          <button className="botao-continuar">Continuar</button>
+          <p className="descricao">
+            Já possui conta?<a href="#"> Faça login</a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default Menu;    
+export default Cadastro;
